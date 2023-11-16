d100ffd43aae7f9f7af8fddda361e17b2e2c9879
Eguelin <eguelin@student.42lyon.fr>
2023-11-16 19:11:28 +0100

    modif main

--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: eguelin <eguelin@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/14 15:00:26 by eguelin           #+#    #+#             */
-/*   Updated: 2023/11/16 18:41:41 by eguelin          ###   ########lyon.fr   */
+/*   Updated: 2023/11/16 19:09:44 by eguelin          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,17 @@
 int main( void )
 {
 	ClapTrap	gura;
-	ClapTrap	caliope("\033[0;90mCaliope\033[0;0m");
-	ClapTrap	amelia("\033[0;33mAmelia\033[0;0m");
-	ClapTrap	kiara(caliope);
+	ClapTrap	amelia("\033[0;33m\033[1mAmelia\033[0;0m");
+	ClapTrap	caliope("\033[0;90m\033[1mCaliope\033[0;0m");
+	ClapTrap	kiara(amelia);
 
-	std::cout << std::endl << "|--------- print caliope ---------|" << std::endl;
-	std::cout << caliope << std::endl;
+	std::cout << std::endl << "|------------- print -------------|" << std::endl;
+	std::cout << "caliope:" << std::endl << caliope << std::endl;
 
 	caliope = gura;
 
-	std::cout << caliope;
+	std::cout << "caliope:" << std::endl << caliope << std::endl;
+	std::cout << "kiara:" << std::endl << kiara << std::endl;
 	std::cout  << "|---------------------------------|" << std::endl << std::endl;
 	std::cout  << "|--------- GURA ATTACK! ---------|" << std::endl;
 
